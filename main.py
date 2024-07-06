a5ec078297298b6eed7c91740c4855caad6bf6a1
Jesus Gollonet <jesusgollonet@gmail.com>
2024-07-06 09:36:31 +0200

    Interactive version. Click 2 random points and stack pixels between

--- main.py
+++ main.py
@@ -6,62 +6,77 @@ import os
 
 load_dotenv()
 
-# load url from environment variable
-
-stream_url = (
-    os.environ.get("STREAM_URL") or "https://www.youtube.com/watch?v=5qap5aO4i9A"
-)
-url = stream_url
-# Create a Streamlink session
-
-# Get the streams for the given URL
-streams = streamlink.streams(stream_url)
-
-print(streams)
-# print(streams["small"])
-# Get the best stream
-best_stream = streams["worst"]
-best_stream_url = best_stream.to_url()
-
-
 slits = []
 
 slit_height = 3
 
+line: list[()] = []
+
+
+# Bresenham's line algorithm to get the points on the line
+def bresenham_line(x0, y0, x1, y1):
+    points = []
+    dx = abs(x1 - x0)
+    dy = abs(y1 - y0)
+    sx = 1 if x0 < x1 else -1
+    sy = 1 if y0 < y1 else -1
+    err = dx - dy
+
+    while True:
+        points.append((x0, y0))
+        if x0 == x1 and y0 == y1:
+            break
+        e2 = err * 2
+        if e2 > -dy:
+            err -= dy
+            x0 += sx
+        if e2 < dx:
+            err += dx
+            y0 += sy
+    return points
+
+
+def mouse_callback(event, x, y, flags, param):
+    global frame_global
+    if event == cv.EVENT_LBUTTONDOWN:
+        if len(line) >= 2:
+            line.clear()
+        line.append((x, y))
+
+
+cv.namedWindow("Frame")
+cv.setMouseCallback("Frame", mouse_callback)
+
 
 def main():
-    cap = cv.VideoCapture(best_stream_url)
+    cap = cv.VideoCapture("media/resized.mp4")
+    pixel_values = []
+    expanded_slit = []
+    pixel_np_array = []
     c = 0
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
         frame_h, frame_w = frame.shape[:2]
-        print(frame_h, frame_w)
-        row = 620
-        slits.append(frame[row : row + slit_height, :])
 
-        cv.line(frame, (0, row - 2), (frame_w, row - 2), (0, 255, 255), 2)
+        if len(line) == 2:
+            pixel_values = [frame[y, x] for x, y in bresenham_line(*line[0], *line[1])]
+            pixel_np_array = np.array(pixel_values, dtype=np.uint8).reshape(-1, 1, 3)
+            expanded_slit.append(pixel_np_array)
+            cv.line(frame, line[0], line[1], (0, 255, 0), 2)
+
+        if len(pixel_values) > 0:
+            # display expanded_slit as an image
+            if len(expanded_slit) > 400:
+                cv.imshow("Current Slit Source", np.hstack(expanded_slit[-400:]))
+            else:
+                cv.imshow("Current Slit Source", np.hstack(expanded_slit))
+
         cv.imshow("Frame", frame)
-        # show the last 340 slits
-        slits_to_show = frame_h // slit_height
-        if c > slits_to_show:
-            cv.imshow("Slits", np.vstack(slits[-slits_to_show:]))
-        else:
-            cv.imshow("Slits", np.vstack(slits))
         if cv.waitKey(1) & 0xFF == ord("q"):
             break
         c += 1
-
-    print(len(slits))
-    # create an image to contain all slits
-    mosaic = np.zeros((len(slits) * slit_height, frame_w, 3), dtype=np.uint8)
-    for i, slit in enumerate(slits):
-        print(i)
-        current_h = i * slit_height
-        mosaic[current_h : current_h + slit_height, :] = slit
-
-    cv.imwrite("media/mosaic.png", mosaic)
 
 
 if __name__ == "__main__":
